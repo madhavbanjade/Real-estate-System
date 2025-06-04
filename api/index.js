14057c500baa278a0fe3d9beccf6da08a48d995e
madhavbanjade <madhavbanjade005@gmail.com>
2025-06-04 18:02:50 +0545

    index

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import userRouter from "../router/user.router.js";
-import authRouter from "../router/auth.user.js";
-import listingRouter from "../router/listing.router.js";
+import userRouter from "./router/user.router.js";
+import authRouter from "./router/auth.user.js";
+import listingRouter from "./router/listing.router.js";
 
 dotenv.config();
 
@@ -13,19 +13,23 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to MongoDB!"))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB error:", err));
 
+// Routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+// Error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   res.status(statusCode).json({ success: false, statusCode, message });
 });
 
+// ✅ Vercel expects a default export that is a function (req, res) => {}
 export default app;
