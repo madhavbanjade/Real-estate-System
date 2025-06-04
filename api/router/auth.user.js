10517c03e8743e80dff2ab08dc29f2690f493d2d
madhavbanjade <madhavbanjade005@gmail.com>
2025-06-04 11:50:07 +0545

    inital commit

---
+++ api/router/auth.user.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {
+  signIn,
+  signOut,
+  signup,
+  
+} from "../controllers/auth.controller.js";
+
+const authRouter = Router();
+authRouter.route("/sign-up").post(signup);
+authRouter.route("/sign-in").post(signIn);
+authRouter.route("/sign-out").get(signOut);
+
+export default authRouter;
